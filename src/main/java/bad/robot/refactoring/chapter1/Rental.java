80b270be2d4009537e944bd15118b77c4c8dcb93
Timo Nurminen <timo.nurminen@gmail.com>
2017-09-09 00:11:09 +0300

    First set of refactors.

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -1,8 +1,5 @@
 package bad.robot.refactoring.chapter1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Rental {
 
     private Movie movie;
@@ -21,4 +18,33 @@ public class Rental {
         return daysRented;
     }
 
+    protected double getCharge() {
+        double amount = 0;
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (this.getDaysRented() > 2)
+                    amount += (this.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (this.getDaysRented() > 3)
+                    amount += (this.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    int getFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
 }
