3a18a63208cc3d81128aa0264dbb5bba46edc827
Timo Nurminen <timo.nurminen@gmail.com>
2017-09-10 16:38:48 +0300

    More refactors.

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -21,24 +21,32 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-
-            totalAmount += rental.getCharge();
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 
         return result;
     }
 
+    public double getTotalAmount(){
+        double totalAmount = 0;
+        for (Rental rental : this.rentals) {
+            totalAmount += rental.getCharge();
+        }
+        return totalAmount;
+    }
+    public int getTotalFrequentRenterPoints(){
+        int frequentRenterPoints = 0;
+        for (Rental rental : this.rentals) {
+            frequentRenterPoints += rental.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
 
 }
